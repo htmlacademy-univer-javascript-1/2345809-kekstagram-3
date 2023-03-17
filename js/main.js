f22ad548f5bafdab3ba61dfa3cd6e15e6d932421
Malik <70970534+maxvell07@users.noreply.github.com>
2023-03-17 17:26:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 function getRandomNumber(min,max){
-  if(min>max){
+ 
+ if(min>max){
     const t=min;
     min=max;
     max=t;
-  }
-    const res = Math.round(Math.random()*(max-min)+min);
+}
+  
+  const res = Math.round(Math.random()*(max-min)+min);
   return res;
+
 }
 
 function checkLengthStr(str, length){
