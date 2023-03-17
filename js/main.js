d61551f036930b41a33f8e4cbae10d7002c76be7
malik <bittiev.malik@mail.ru>
2023-03-17 17:35:38 +0300

    fix2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-function getRandomNumber(min,max){
-  if(min>max){
-    const t=min;
-    min=max;
-    max=t;
-  }
-    const res = Math.round(Math.random()*(max-min)+min);
-  return res;
+function getRandomNumber(min, max) {
+	if (min > max) {
+		const t = min;
+		min = max;
+		max = t;
+	}
+	const res = Math.round(Math.random() * (max - min) + min);
+	return res;
 }
 
-function checkLengthStr(str, length){
-  return str.length <=length;
+function checkLengthStr(str, length) {
+	return str.length <= length;
 }
