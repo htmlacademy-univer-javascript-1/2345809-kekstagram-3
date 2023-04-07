d45073432072c7a5f988d7098a41f1b52e1b9ba0
malik <bittiev.malik@mail.ru>
2023-04-07 10:59:58 +0300

    module5-task1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { checkLengthStr } from './util.js';
 import { getPhotos } from './data.js';
 
-console.log(checkLengthStr('zzzz', 6));
-console.log(getPhotos());
+checkLengthStr('zzzz', 6);
+getPhotos();
